5281cb84c7e0c8bbd140ff5facb3da6d8c7ec42f
Stephen Austin <austin.stephen@gmail.com>
2026-01-15 17:32:16 -0800

    updated journal.js with a 3rd function.

--- journal.js
+++ journal.js
@@ -7,3 +7,10 @@ function greet(name) {
 function square(x) {
   return x * x;
 }
+
+// Hello to Harry, then goodbye
+function greet(who) {
+  console.log("Hello " + who);
+}
+greet("Harry");
+console.log("Bye");
